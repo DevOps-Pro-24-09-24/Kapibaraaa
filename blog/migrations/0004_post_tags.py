8d658139f796e9e1640fa693086e4a810f131469
kappibara <sotishvili.alexander@gmail.com>
2024-12-18 07:53:48 +0200

    files formatted after black

--- blog/migrations/0004_post_tags.py
+++ blog/migrations/0004_post_tags.py
@@ -7,14 +7,19 @@ import taggit.managers
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('taggit', '0003_taggeditem_add_unique_index'),
-        ('blog', '0003_comment_comment_blog_commen_created_0e6ed4_idx'),
+        ("taggit", "0003_taggeditem_add_unique_index"),
+        ("blog", "0003_comment_comment_blog_commen_created_0e6ed4_idx"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='post',
-            name='tags',
-            field=taggit.managers.TaggableManager(help_text='A comma-separated list of tags.', through='taggit.TaggedItem', to='taggit.Tag', verbose_name='Tags'),
+            model_name="post",
+            name="tags",
+            field=taggit.managers.TaggableManager(
+                help_text="A comma-separated list of tags.",
+                through="taggit.TaggedItem",
+                to="taggit.Tag",
+                verbose_name="Tags",
+            ),
         ),
     ]
--- blog/tests.py
+++ blog/tests.py
@@ -17,18 +17,14 @@ class AddCommentViewTests(TestCase):
         self.assertContains(response, "<h1>Add Book</h1>", html=True)
 
     def test_post_success(self):
-        url = reverse('')
-        response = self.client.post(
-            "/books/add/", data={"title": "Dombey and Son"}
-        )
+        url = reverse("")
+        response = self.client.post("/books/add/", data={"title": "Dombey and Son"})
 
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.assertEqual(response["Location"], "/books/")
 
     def test_post_error(self):
-        response = self.client.post(
-            "/books/add/", data={"title": "Dombey & Son"}
-        )
+        response = self.client.post("/books/add/", data={"title": "Dombey & Son"})
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertContains(response, "Use 'and' instead of '&'", html=True)
